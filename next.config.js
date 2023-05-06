1d96dd280a1dbfbd4da7b16290dd1ef23a019d7d
Mrinal <mrinalbitsat@gmail.com>
2023-05-06 16:16:11 +0530

    Done with the clone

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental : {
+    appDir : true,
+  },
   images : {
-    domains : ['pbs.twimg.com', 'images.unsplash.com']
+    domains : ['pbs.twimg.com', 'images.unsplash.com', 'avatars.githubusercontent.com']
   },
 }
 
